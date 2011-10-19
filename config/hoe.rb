0af04e34304cf6f2592a13531782b17687a92d33
zzzhc <zzzhc.starfire@gmail.com>
2011-10-19 22:57:19 +0800

    merge v0.2.2 from http://rubyforge.org/frs/?group_id=6324&release_id=24867

--- config/hoe.rb
+++ config/hoe.rb
@@ -31,7 +31,7 @@ end
 REV = nil 
 # UNCOMMENT IF REQUIRED: 
 # REV = `svn info`.each {|line| if line =~ /^Revision:/ then k,v = line.split(': '); break v.chomp; else next; end} rescue nil
-VERS = Rqr::VERSION::STRING + (REV ? ".#{REV}" : "")
+VERS = RQR::VERSION::STRING + (REV ? ".#{REV}" : "")
 RDOC_OPTS = ['--quiet', '--title', 'rqr documentation',
     "--opname", "index.html",
     "--line-numbers", 
